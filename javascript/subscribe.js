c5007a1ac2c59ececb77d1c6f3e2871847e3575a
Jonathan Potter <jonathan.potter.business@gmail.com>
2015-08-30 20:20:50 -0700

    adds skeleton for subscribe

---
+++ javascript/subscribe.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let subscribedProperties = {};
+
+let subscriptions = {};
+
+export default (propertyList, callback) => {
+  /* make a subscription */
+
+  /* add the subscription to the subscriptions object */
+
+  /* add references to the subscription to each of the */
+  /* subscribed properties */
+};
---
+++ test/subscribeSpec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import subscribe from 'javascript/subscribe';
+
+describe('subscribe', () => {
+
+});
